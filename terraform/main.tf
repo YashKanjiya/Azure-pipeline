554411a779e8fb8abd69d91cc06e5540c81301f3
YASH KANJIYA <70482112+YashKanjiya@users.noreply.github.com>
2024-04-08 17:56:22 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,7 @@ resource "random_password" "password" {
 
 # This creates a MySQL server
 resource "azurerm_mysql_server" "main" {
-  name                              = "${azurerm_resource_group.main.name} mysql-server"
+  name                              = "${azurerm_resource_group.main.name}-mysql-server"
   location                          = azurerm_resource_group.main.location
   resource_group_name               = azurerm_resource_group.main.name
 
